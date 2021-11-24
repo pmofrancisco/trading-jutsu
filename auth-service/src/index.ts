0515ddaedc6c28dc685cae5960f0524adb9f837e
Paul Michael Francisco <pmofrancisco@yahoo.com>
2021-11-24 15:27:07 +0800

    Email and password validation

--- auth-service/src/index.ts
+++ auth-service/src/index.ts
@@ -2,11 +2,13 @@ import express from 'express';
 import { json } from 'body-parser';
 
 import { currentUserRouter } from './routes/current-user';
+import { signupRouter } from './routes/signup';
 
 const app = express();
 app.use(json());
 
 app.use(currentUserRouter);
+app.use(signupRouter);
 
 app.listen(3000, () => {
   console.log('Listening on port 3000');
