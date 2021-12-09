94186a5adf5ad2b20fdc11aa83c78b0614db251f
Paul Michael Francisco <pmofrancisco@yahoo.com>
2021-12-09 15:10:02 +0800

    Current user route

--- auth-service/src/routes/current-user.ts
+++ auth-service/src/routes/current-user.ts
@@ -1,9 +1,19 @@
 import express from 'express';
+import jwt from 'jsonwebtoken';
 
 const router = express.Router();
 
 router.get('/api/users/current-user', (req, res) => {
-  res.send({ currentUser: 'pmofrancisco' })
+  if (!req.session?.jwt) {
+    return res.send({ currentUser: null });
+  }
+
+  try {
+    const payload = jwt.verify(req.session.jwt, process.env.TJ_JWT_KEY!);
+    res.send({ currentUser: payload });
+  } catch(err) {
+    res.send({ currentUser: null });
+  }
 });
 
 export { router as currentUserRouter };
