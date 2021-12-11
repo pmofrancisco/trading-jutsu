a5d865be82695ef85c7203f5faf3e9cc0f02d191
Paul Michael Francisco <pmofrancisco@yahoo.com>
2021-12-11 13:03:24 +0800

    Current user test

--- auth-service/src/routes/__test__/current-user.test.ts
+++ auth-service/src/routes/__test__/current-user.test.ts
@@ -12,3 +12,12 @@ it('responds with the details of the current user', async () => {
 
   expect(response.body.currentUser.email).toEqual('test@test.com');
 });
+
+it('responds with null if not authenticated', async () => {
+  const response = await request(app)
+    .get('/api/users/current-user')
+    .send()
+    .expect(200);
+
+  expect(response.body.currentUser).toEqual(null);
+});
