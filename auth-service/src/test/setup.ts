5148e161229c25bba7d34cf68de460a1ad8730a7
Paul Michael Francisco <pmofrancisco@yahoo.com>
2021-12-11 13:00:56 +0800

    Create auth cookie helper

--- auth-service/src/test/setup.ts
+++ auth-service/src/test/setup.ts
@@ -1,6 +1,11 @@
 import { MongoMemoryServer } from 'mongodb-memory-server';
 import mongoose from 'mongoose';
+import request from 'supertest';
 import { app } from '../app';
+
+declare global {
+  var signup: () => Promise<string[]>
+}
 
 let mongo: MongoMemoryServer;
 
@@ -23,3 +28,15 @@ afterAll(async () => {
   await mongo.stop();
   await mongoose.connection.close();
 });
+
+global.signup = async () => {
+  const email = 'test@test.com';
+  const password = 'password';
+
+  const response = await request(app)
+    .post('/api/users/signup')
+    .send({ email, password })
+    .expect(201);
+
+  return response.get('Set-Cookie');
+};
