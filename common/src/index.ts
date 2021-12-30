163053bd609312a43fc7ea045e707063052b36ac
Paul Michael Francisco <pmofrancisco@yahoo.com>
2021-12-30 19:06:30 +0800

    Updates

--- common/src/index.ts
+++ common/src/index.ts
@@ -1,9 +1,11 @@
-interface User {
-  name: string;
-}
+export * from './errors/bad-request-error';
+export * from './errors/custom-error';
+export * from './errors/database-connection-error';
+export * from './errors/not-authorized';
+export * from './errors/not-found-error';
+export * from './errors/request-validation-error';
 
-const newUser = {
-  name: 'Paul Michael Francisco'
-};
-
-console.log('newUser', newUser);
+export * from './middlewares/current-user';
+export * from './middlewares/error-handler';
+export * from './middlewares/require-auth';
+export * from './middlewares/validate-request';
