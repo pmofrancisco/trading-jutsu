c581e0a59bf98e086c1a7e5048165b6b168a4434
Paul Michael Francisco <pmofrancisco@yahoo.com>
2022-01-02 12:56:27 +0800

    Initial commit

--- journal-service/src/index.ts
+++ journal-service/src/index.ts
@@ -6,20 +6,20 @@ const start = async () => {
   if (!process.env.TJ_JWT_KEY) {
     throw new Error('TJ_JWT_KEY must be defined');
   }
-  if (!process.env.TJ_AUTH_MONGO_URI) {
-    throw new Error('TJ_AUTH_MONGO_URI must be defined');
+  if (!process.env.TJ_JOURNAL_MONGO_URI) {
+    throw new Error('TJ_JOURNAL_MONGO_URI must be defined');
   }
-  if (!process.env.TJ_AUTH_PORT) {
-    throw new Error('TJ_AUTH_PORT must be defined');
+  if (!process.env.TJ_JOURNAL_PORT) {
+    throw new Error('TJ_JOURNAL_PORT must be defined');
   }
   try {
-    await mongoose.connect(process.env.TJ_AUTH_MONGO_URI);
+    await mongoose.connect(process.env.TJ_JOURNAL_MONGO_URI);
     console.log('Connected to MongoDB');
   } catch (err) {
     throw new DatabaseConnectionError();
   }
-  app.listen(process.env.TJ_AUTH_PORT, () => {
-    console.log(`Listening on port ${process.env.TJ_AUTH_PORT}`);
+  app.listen(process.env.TJ_JOURNAL_PORT, () => {
+    console.log(`Listening on port ${process.env.TJ_JOURNAL_PORT}`);
   });
 };
 
