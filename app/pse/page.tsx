78d3dcb401c9ab9ad316ac8b71fff13bee29c56e
Paul Michael Francisco <pmofrancisco@yahoo.com>
2026-04-11 09:27:01 +0800

    Create crypto and pse pages

---
+++ app/pse/page.tsx
@@ -0,0 +1,5 @@
+export default function Pse() {
+  return (
+    <div>PSE Page</div>
+  );
+}
---
+++ components/header.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+export default function Header() {
+  return (
+    <div className="flex gap-4 p-2">
+      <Link href="/">Home</Link>
+      <Link href="/pse">PSE</Link>
+      <Link href="/crypto">Crypto</Link>
+    </div>
+  );
+}
