c62621869cb0657e2bc97fc5fd4379fcf6fd5ebd
Paul Michael Francisco <pmofrancisco@yahoo.com>
2026-04-21 21:14:45 +0800

    Implement auth

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,31 @@
-export default function Home() {
+import { auth, signIn, signOut } from "@/auth"
+import { Button } from "@heroui/react";
+
+export default async function Home() {
+  const session = await auth();
+
   return (
-    <div>Home Page</div>
+    <div>
+      <form
+        action={async () => {
+          "use server"
+          await signIn("github")
+        }}
+      >
+        <Button type="submit">Signin with GitHub</Button>
+      </form>
+      <br />
+      <form
+        action={async () => {
+          "use server"
+          await signOut()
+        }}
+      >
+        <Button type="submit">Signout</Button>
+      </form>
+      <br />
+      <div>{session?.user ? JSON.stringify(session.user) : "Not signed in"}</div>
+    </div>
+    
   );
 }
